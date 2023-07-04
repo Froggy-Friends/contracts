4b7acd6c85bf302f2339aecdd74d465c84fd79de
Alfonso <alfonso@viruett.com>
2023-07-04 03:31:36 -0700

    updates hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,52 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-chai-matchers";
+import "@openzeppelin/hardhat-upgrades";
+import dotenv from "dotenv";  
+dotenv.config();
+
+const { ALCHEMY_API_URL_STG, ALCHEMY_API_URL, PRIVATE_KEY, ETHERSCAN_API_KEY, COINMARKETCAP_API_KEY } = process.env;
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+  solidity: {
+    version: "0.8.18",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 800,
+      },
+    },
+  },
+  defaultNetwork: "goerli",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    mainnet: {
+      url: ALCHEMY_API_URL,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+    goerli: {
+      url: ALCHEMY_API_URL_STG,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+    coverage: {
+      url: "http://127.0.0.1:8555"
+    }
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    currency: 'USD',
+    enabled: true,
+    coinmarketcap: COINMARKETCAP_API_KEY,
+    gasPrice: 50
+  },
+  typechain: {
+    outDir: "types",
+    target: "ethers-v5"
+  }
 };
 
 export default config;
