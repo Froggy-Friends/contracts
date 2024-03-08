78dc7bb9fbee71072e01daea3ab0978969409313
0xFonzy <fonzy@fonzy.io>
2024-03-08 01:13:23 -0800

    adds code comments

--- contracts/FroggyFriends.sol
+++ contracts/FroggyFriends.sol
@@ -99,119 +99,177 @@ contract FroggyFriends is OwnableUpgradeable, DefaultOperatorFiltererUpgradeable
     // =============================================================
 
     modifier ifNotHibernating(uint256 _tokenId) {
-        require(lockStatus[ownerOf(_tokenId)] == HibernationStatus.AWAKE, "this token has been locked");
+        require(lockStatus[ownerOf(_tokenId)] == HibernationStatus.AWAKE, "Frog is currently Hibernating.");
         _;
     }
 
     modifier checkHibernationIsAvailable(HibernationStatus _hibernationStatus) {
-        require(lockStatusAvailability[_hibernationStatus], "_hibernationStatus is not available");
+        require(lockStatusAvailability[_hibernationStatus], "Not a valid Hibernation duration.");
         _;
     }
 
-    // considerations : gas cost should be small
+    /**
+     * Hibernates for the chosen amount of time
+     * @param _hibernationStatus 1 = 30 days, 2 = 60 days, 3 = 90 days
+     * @return true when hibernation is complete
+     */
     function hibernate(HibernationStatus _hibernationStatus) public checkHibernationIsAvailable(_hibernationStatus) returns (bool) {
         uint256 _balances = balanceOf(msg.sender);
-        require(_balances > 0, "you don't have any tokens");
+        require(_balances > 0, "Frog balance is zero.");
         lockStatus[msg.sender] = _hibernationStatus;
         HibernationDate[msg.sender] = block.timestamp;
         emit LogHibernate(msg.sender, block.timestamp, _hibernationStatus);
         return true;
     }
 
-    function unlockMyTokens() public returns (bool) {
-        require(HibernationDate[msg.sender] > 0, "your tokens have not been locked");
-        require(block.timestamp > getUnlockTimestamp(msg.sender), "awake time has'nt been reached");
-        uint256 _tadpoleAmount = _calculateTotalRewardAmount();
-        //the total amount should be tested and ceck if it is in compliance with the expectations
+    /**
+     * Wakes frogs from hibernation and distributes tadpole rewards to holder
+     * @return true when wake is complete
+     */
+    function wake() public returns (bool) {
+        require(HibernationDate[msg.sender] > 0, "You are not currently Hibernating.");
+        require(block.timestamp > getUnlockTimestamp(msg.sender), "Your Hibernation period has not ended.");
+        uint256 _tadpoleAmount = _calculateTotalRewardAmount(); // calculate total reward amount includes base rate + boosts
         tadpole.transferFrom(tadpoleSender, msg.sender, _tadpoleAmount);
         lockStatus[msg.sender] = HibernationStatus.AWAKE;
         emit LogAwake(msg.sender, block.timestamp, _tadpoleAmount);
         return true;
     }
 
-    // the result will be returned in seconds
+    /**
+     * @param _account address of holder to query
+     * @return lockDuration total hibernation duration in seconds
+     */
     function getLockDuration(address _account) public view returns (uint256) {
         return (uint8(lockStatus[_account]) * 30) * (24 * 60 * 60); // (number of hibernate days) * each day
     }
 
-    // the result will be returned in seconds
+    /**
+     * @param _account address of holder to query
+     * @return unlockTimestamp hibernation completion date in seconds
+     */
     function getUnlockTimestamp(address _account) public view returns (uint256) {
         return getLockDuration(_account) + HibernationDate[_account];
     }
 
-    // no froggy rarity tiers . all of them are flat
-    //total rewards = base * total frogs * boosts
+    /**
+     * Calculates total rewards including all frogs held * (base rate + boosts)
+     */
     function _calculateTotalRewardAmount() internal view returns (uint256) {
         return _getRewardPerFroggy() * balanceOf(msg.sender);
     }
 
+    /**
+     * Calculates tadpole rewards per frog including (base rate + boosts)
+     */
     function _getRewardPerFroggy() internal view returns (uint256) {
         return (statusTadpoleAmount[lockStatus[msg.sender]]) + _calculateTotalBoostedTadpoles();
     }
 
-    //as the _rate parameter for setBoostRate function shouldnt be entered in percentage, here we divide the hole amount by 100
+    /**
+     * Calculates total tadpole boost including (hibernation duration rate * boost) / 100.
+     * The hibernation duration (30,60,90) rate should be a flat number.
+     */
     function _calculateTotalBoostedTadpoles() internal view returns (uint256) {
         return (statusTadpoleAmount[lockStatus[msg.sender]] * _calculateTotalBoost()) / 100;
     }
 
-    // should figure out a way to reduce gas cost
+    /**
+     * Calculates total boost percentage for the holder by adding all available boost percentages.
+     */
     function _calculateTotalBoost() internal view returns (uint256) {
-        // Golden Lily Pad
-        uint256 ribbitItemBoost = TokenBoostRate[address(ribbitItem)][1] * ribbitItem.balanceOf(msg.sender, 1);
-        //Froggy Minter Soulbound
-        uint256 froggyMinterBoost = TokenBoostRate[address(froggySoulbounds)][1] * froggySoulbounds.balanceOf(msg.sender, 1);
-        //One Year Anniversary Holder Soulbound
-        uint256 oneYearAnniversaryBoost = TokenBoostRate[address(froggySoulbounds)][2] * froggySoulbounds.balanceOf(msg.sender, 2);
+        uint256 ribbitItemBoost = TokenBoostRate[address(ribbitItem)][1] * ribbitItem.balanceOf(msg.sender, 1); // Golden Lily Pad
+        uint256 froggyMinterBoost = TokenBoostRate[address(froggySoulbounds)][1] * froggySoulbounds.balanceOf(msg.sender, 1); //Froggy Minter Soulbound
+        uint256 oneYearAnniversaryBoost = TokenBoostRate[address(froggySoulbounds)][2] * froggySoulbounds.balanceOf(msg.sender, 2); //One Year Anniversary Holder Soulbound
         return ribbitItemBoost + froggyMinterBoost + oneYearAnniversaryBoost;
     }
 
-    //argument "_amount" should be cosnidered as 16 decimals
-    // argument "_amount" should be multiplied by 10**16
+    /**
+     * Sets the hibernation duration rate
+     * @param _status the hibernation duration i.e. 1 = 30 days, 2 = 60 days, 3 = 90 days
+     * @param _amount the tadpole base rate for the hibernation duration
+     */
     function setTadpoleReward(HibernationStatus _status, uint256 _amount) public onlyOwner {
+        // argument "_amount" should be considered as 16 decimals
+        // argument "_amount" should be multiplied by 10**16
         statusTadpoleAmount[_status] = _amount * 100;
     }
 
-    // argument "_rate" should not be in percentage.
-    //example: for 10%  the argument for "_rate" should be 10
+    /**
+     * Sets the boost rate
+     * @param _address the boost contract address
+     * @param _tokenId the boost token id
+     * @param _rate the boost rate flat number i.e. set 10 for 10% representation
+     */
     function setBoostRate(address _address, uint256 _tokenId, uint256 _rate) public onlyOwner {
+        // argument "_rate" should not be in percentage.
+        // example: for 10%  the argument for "_rate" should be 10
         TokenBoostRate[_address][_tokenId] = _rate;
     }
 
-    // by default all values for lockStatusAvailability are false(unavailable)
-    //invoke this function to make all status available
-    function setAllHibernationsAvailable() public onlyOwner {
-        lockStatusAvailability[HibernationStatus.THIRTYDAY] = true;
-        lockStatusAvailability[HibernationStatus.SIXTYDAY] = true;
-        lockStatusAvailability[HibernationStatus.NINETYDAY] = true;
+    /**
+     * Enable or disable all hibernation duration options
+     * @param _status set to true to enable all hibernation options, false to disable them
+     */
+    function setAllHibernationsAvailable(bool _status) public onlyOwner {
+        lockStatusAvailability[HibernationStatus.THIRTYDAY] = _status;
+        lockStatusAvailability[HibernationStatus.SIXTYDAY] = _status;
+        lockStatusAvailability[HibernationStatus.NINETYDAY] = _status;
     }
 
-    //invoke this function to make Ninetyday hibernation unavailable when _NINETYDAY is false
-    function setNinetydayAvailable(bool _NINETYDAY) public onlyOwner {
-        lockStatusAvailability[HibernationStatus.NINETYDAY] = _NINETYDAY;
+    /**
+     * Enable or disable the 90 day hibernation option
+     * @param _status set to true to enable 90 day hibernation, false to disable the option
+     */
+    function setNinetydayAvailable(bool _status) public onlyOwner {
+        lockStatusAvailability[HibernationStatus.NINETYDAY] = _status;
     }
 
-    //invoke this function to make Sixtyday hibernation unavailable when _SIXTYDAY is false
-    function setSixtydayAvailable(bool _SIXTYDAY) public onlyOwner {
-        lockStatusAvailability[HibernationStatus.SIXTYDAY] = _SIXTYDAY;
+    /**
+     * Enable or disable the 60 day hibernation option
+     * @param _status set to true to enable 60 day hibernation, false to disable the option
+     */
+    function setSixtydayAvailable(bool _status) public onlyOwner {
+        lockStatusAvailability[HibernationStatus.SIXTYDAY] = _status;
     }
 
-    //invoke this function to make Thirtyday hibernation unavailable when _THIRTYDAY is false
-    function setThirtydayAvailable(bool _THIRTYDAY) public onlyOwner {
-        lockStatusAvailability[HibernationStatus.THIRTYDAY] = _THIRTYDAY;
+    /**
+     * Enable or disable the 30 day hibernation option
+     * @param _status set to true to enable 30 day hibernation, false to disable the option
+     */
+    function setThirtydayAvailable(bool _status) public onlyOwner {
+        lockStatusAvailability[HibernationStatus.THIRTYDAY] = _status;
     }
 
+    /**
+     * Sets the ribbit items contract address
+     * @param _ribbitItem the contract address of the ribbit items collection
+     */
     function setRibbitItemContract(address _ribbitItem) public onlyOwner {
         ribbitItem = IRibbitItem(_ribbitItem);
     }
 
+    /**
+     * Sets the froggy soulbounds contract address
+     * @param _froggySoulbounds the contract address of the froggy soulbounds collection
+     */
     function setFroggySoulboundsContract(address _froggySoulbounds) public onlyOwner {
         froggySoulbounds = IFroggySoulbounds(_froggySoulbounds);
     }
 
+    /**
+     * Sets the tadpoles contract addresss
+     * @param _tadpole the contract address of the tadpoles collection
+     */
     function setTadpoleContract(address _tadpole) public onlyOwner {
         tadpole = ITadpole(_tadpole);
     }
 
+    /**
+     * Sets the address that will distribute tadpole rewards
+     * The sender must approve this contract to spend tokens
+     * @param _sender address holding tadpole rewards
+     */
     function setTadpoleSender(address _sender) public onlyOwner {
         tadpoleSender = _sender;
     }
