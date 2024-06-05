02029cec34101145b87335f348074fa20c550b57
0xFonzy <fonzy@fonzy.io>
2024-06-05 01:30:59 -0700

    add lz endpoint constants

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,40 +1,40 @@
 import { ethers, run, upgrades } from "hardhat";
 
-
-function sleep(ms: number) {
-    return new Promise(resolve => {
-        setTimeout(resolve, ms);
-    })
-}
+const lzMainnet = "0x66A71Dcef29A0fFBDBE3c6a460a3B5BC225Cd675";
+const lzSepolia = "0x7cacBe439EaD55fa1c22790330b12835c6884a91";
+const lzHolesky = "0x4e08B1F1AC79898569CfB999FB92B5495FB18A2B";
+const lzBase = "0xb6319cC6c8c27A8F5dAF0dD3DF91EA35C4720dd7";
 
 async function main() {
-    console.log("Starting deployment...");
-    const FroggyFriends = await ethers.getContractFactory("FroggyFriends");
-    const [owner] = await ethers.getSigners();
-    console.log("\nDeployment Owner: ", owner.address);
+  console.log("Starting deployment...");
+  const FroggyFriends = await ethers.getContractFactory("base/FroggyFriends");
+  const [owner] = await ethers.getSigners();
+  console.log("\nDeployment Owner: ", owner.address);
 
-    const _minGasToTransfer = 100000;
-    const _lzEndpoint = '0x66A71Dcef29A0fFBDBE3c6a460a3B5BC225Cd675'; //mainnet
-    const froggyFriends = (await upgrades.deployProxy(FroggyFriends, [_minGasToTransfer, _lzEndpoint]));
-    console.log("\nContract Address: ", froggyFriends.address);
+  const _minGasToTransfer = 100000;
+  const _lzEndpoint = lzMainnet;
+  const froggyFriends = await upgrades.deployProxy(FroggyFriends, [
+    _minGasToTransfer,
+    _lzEndpoint,
+  ]);
+  console.log("\nContract Address: ", froggyFriends.address);
 
-    await froggyFriends.deployed();
-    console.log("\nContract deployed...");
+  await froggyFriends.deployed();
+  console.log("\nContract deployed...");
 
-    await froggyFriends.deployTransaction.wait(5);
-    console.log("\nContract deployed with 5 confirmations");
+  await froggyFriends.deployTransaction.wait(5);
+  console.log("\nContract deployed with 5 confirmations");
 
-    console.log('\Verifying contract code on Etherscan...');
-    await run("verify:verify",
-    {
-        address: froggyFriends.address,
-        constructorArguments: []
-    });
+  console.log("Verifying contract code on Etherscan...");
+  await run("verify:verify", {
+    address: froggyFriends.address,
+    constructorArguments: [],
+  });
 }
 
 main()
-    .then(() => process.exit(0))
-    .catch(error => {
-        console.log(error);
-        process.exit(1);
-    });
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.log(error);
+    process.exit(1);
+  });
