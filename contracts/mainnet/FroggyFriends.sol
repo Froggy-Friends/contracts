0e972fe1a920527e68c467848acb7e6f1409c62c
0xFonzy <fonzy@fonzy.io>
2024-12-17 00:26:53 -0800

    removes limit break contracts

--- contracts/mainnet/FroggyFriends.sol
+++ contracts/mainnet/FroggyFriends.sol
@@ -12,19 +12,12 @@ import {ONFT721Upgradeable} from "@layerzerolabs/solidity-examples/contracts/con
 import {IONFT721Upgradeable} from "@layerzerolabs/solidity-examples/contracts/contracts-upgradable/token/onft/ERC721/IONFT721Upgradeable.sol";
 import {MerkleProofUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/cryptography/MerkleProofUpgradeable.sol";
 import {ITadpole} from "./Interfaces.sol";
-import {CreatorTokenBase} from "@limitbreak/creator-token-contracts/contracts/utils/CreatorTokenBase.sol";
-import {ICreatorToken} from "@limitbreak/creator-token-contracts/contracts/interfaces/ICreatorToken.sol";
-import {ContextUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/ContextUpgradeable.sol";
-import {Context} from "@openzeppelin/contracts/utils/Context.sol";
-
-error InvalidSize();
 
 contract FroggyFriends is
     OwnableUpgradeable,
     DefaultOperatorFiltererUpgradeable,
     ERC2981Upgradeable,
-    ONFT721Upgradeable,
-    CreatorTokenBase
+    ONFT721Upgradeable
 {
     string public froggyUrl;
     uint256 public maxSupply;
@@ -85,69 +78,6 @@ contract FroggyFriends is
             );
     }
 
-    // =============================================================
-    //                      ERC2981 OVERRIDES
-    // =============================================================
-
-    function approve(
-        address operator,
-        uint256 tokenId
-    )
-        public
-        override(ERC721Upgradeable, IERC721Upgradeable)
-        onlyAllowedOperatorApproval(operator)
-    {
-        super.approve(operator, tokenId);
-    }
-
-    function setApprovalForAll(
-        address operator,
-        bool approved
-    )
-        public
-        override(ERC721Upgradeable, IERC721Upgradeable)
-        onlyAllowedOperatorApproval(operator)
-    {
-        super.setApprovalForAll(operator, approved);
-    }
-
-    function transferFrom(
-        address from,
-        address to,
-        uint256 tokenId
-    )
-        public
-        override(ERC721Upgradeable, IERC721Upgradeable)
-        onlyAllowedOperator(from)
-    {
-        super.transferFrom(from, to, tokenId);
-    }
-
-    function safeTransferFrom(
-        address from,
-        address to,
-        uint256 tokenId
-    )
-        public
-        override(ERC721Upgradeable, IERC721Upgradeable)
-        onlyAllowedOperator(from)
-    {
-        super.safeTransferFrom(from, to, tokenId);
-    }
-
-    function safeTransferFrom(
-        address from,
-        address to,
-        uint256 tokenId,
-        bytes memory data
-    )
-        public
-        override(ERC721Upgradeable, IERC721Upgradeable)
-        onlyAllowedOperator(from)
-    {
-        super.safeTransferFrom(from, to, tokenId, data);
-    }
-
     function supportsInterface(
         bytes4 interfaceId
     )
@@ -158,39 +88,5 @@ contract FroggyFriends is
         returns (bool)
     {
         return super.supportsInterface(interfaceId);
-    }
-
-    /// @dev Ties the open-zeppelin _beforeTokenTransfer hook to more granular transfer validation logic
-    function _beforeTokenTransfer(
-        address from,
-        address to,
-        uint256 firstTokenId,
-        uint256 batchSize
-    ) internal virtual override {
-        if (batchSize > 1) revert InvalidSize();
-        _validateBeforeTransfer(from, to, firstTokenId);
-    }
-
-    /// @dev Ties the open-zeppelin _afterTokenTransfer hook to more granular transfer validation logic
-    function _afterTokenTransfer(
-        address from,
-        address to,
-        uint256 firstTokenId,
-        uint256 batchSize
-    ) internal virtual override {
-        if (batchSize > 1) revert InvalidSize();
-        _validateAfterTransfer(from, to, firstTokenId);
-    }
-
-    function _requireCallerIsContractOwner() internal view virtual override {
-        _checkOwner();
-    }
-
-    function _msgData() internal view virtual override(ContextUpgradeable, Context) returns (bytes calldata) {
-        return ContextUpgradeable._msgData();
-    }
-
-    function _msgSender() internal view virtual override(ContextUpgradeable, Context) returns (address) {
-        return ContextUpgradeable._msgSender();
     }
 }
